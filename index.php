9119d60c36cb1fd69790c27e63127f714526c4ef
Roman3349 <ondracek.roman@centrum.cz>
2014-12-31 19:29:12 +0100

    Kostra webu

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang='cs'>
+    <head>
+        <base href='/'>
+        <meta charset='UTF-8'>
+        <meta name='author' content='Roman Ondráček'>
+        <meta name='robots' content='index, follow'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <link href='style.css' rel='stylesheet' type='text/css'>
+        <title>Roman Ondráček</title>
+    </head>
+    <body>
+        <header>
+            <h1>Roman Ondráček</h1>
+            <h2>Student, programátor, kodér</h2>
+        </header>
+        <nav>
+            <ul>
+                <li><a href='/'>O mně</a></li>
+                <li><a href='#co-umim'>Co umím</a></li>
+                <li><a href='#reference'>Reference</a></li>
+                <li><a href='#kontakt'>Kontakt</a></li>
+            </ul>
+        </nav>
+        <section>
+            <header>O mně</header>
+            <article>
+                <p>Vítejte na mém webu. Jmenuji se Roman Ondráček a je mi 16 let. Jsem kodér, programátor a student 
+                    <a href='http://www.gymbos.cz/'>Gymnázia Boskovice</a>. 
+                    V podledních měsících se věnuji vývoji webových aplikacích.
+                    Účastním se projektu <a href='http://wifileaks.cz/'>Wifileaks</a>
+                    a naleznete mě tam pod přezdívkou Roman3349.</p>
+            </article>
+            <header id="co-umim">Co umím</header>
+            <article>
+
+            </article>
+            <header id="reference">Reference</header>
+            <article>
+                <div class="reference">
+                    <h3><a href="http://www.aikidoboskovice.cz/">Aikido Boskovice</a></h3>
+                    <a href="reference/aikidoboskovice.png"><img src="reference/aikidoboskovice_nahled.png"></a>
+                    <p>Tomuto webu jsem dělal vlastní redakční systém a šablonu, kterou jsem dělal pomocí CSS frameworku Bootstrap.
+                        Redakční systém je napojen na MySQL a je objektový.</p>
+                    <p>Použité technologie: HTML5, CSS3, PHP (OOP) a MySQL</p>
+                    <br style="clear: both">
+                </div>
+                <div class="reference">
+                    <h3><a href="http://www.funparba.eu/">FunParba</a></h3>
+                    <a href="reference/funparba.png"><img src="reference/funparba_nahled.png"></a>
+                    <p>Tomuto webu jsem dělal vlastní redakční systém a šablonu podle návrhu majitele webu.
+                        Redakční systém je napojen na MySQL a je objektový.</p>
+                    <p>Použité technologie: HTML5, CSS3, PHP (OOP) a MySQL</p>
+                    <br style="clear: both">
+                </div>
+                <div class="reference">
+                    <h3><a href="http://www.armycraft.eu/">ArmyCraft</a></h3>
+                    <a href="reference/armycraft.png"><img src="reference/armycraft_nahled.png"></a>
+                    <p>Tomuto webu jsem dělal vlastní redakční systém a šablonu.
+                        Redakční systém je napojen na MySQL a je objektový.</p>
+                    <p>Použité technologie: HTML5, CSS3, PHP (OOP) a MySQL</p>
+                    <br style="clear: both">
+                </div>
+                <div class="reference">
+                    <h3><a href="http://www.crazymine.eu/">CrazyMine</a></h3>
+                    <a href="reference/crazymine.png"><img src="reference/crazymine_nahled.png"></a>
+                    <p>Tomuto webu jsem dělal šablonu.</p>
+                    <p>Použité technologie: HTML5, CSS3</p>
+                    <br style="clear: both">
+                </div>
+                <div class="reference">
+                    <h3><a href="#">SafariCraft</a></h3>
+                    <a href="reference/safaricraft.png"><img src="reference/safaricraft_nahled.png"></a>
+                    <p>Tomuto webu jsem dělal šablonu pro redakční systém Wordpress.</p>
+                    <p>Použité technologie: HTML5, CSS3</p>
+                    <br style="clear: both">
+                </div>
+                <div class="reference">
+                    <h3><a href="#">Black Servers</a></h3>
+                    <a href="reference/black-servers.png"><img src="reference/black-servers_nahled.png"></a>
+                    <p>Tomuto webu jsem dělal šablonu podle návrhu majitele webu.</p>
+                    <p>Použité technologie: HTML5, CSS3</p>
+                    <br style="clear: both">
+                </div>
+            </article>
+            <header id="kontakt">Kontakt</header>
+            <article>
+
+            </article>
+        </section>
+        <footer>&copy; 2014 Roman Ondráček</footer>
+    </body>
+</html>
